865970266ab3b600367d36031eb8882d3a3a6535
Madhu Reddy <madhusudhanreddola@gmail.com>
2022-05-30 10:12:41 -0400

    Update in-memory-service.service.ts

--- src/app/shared/services/in-memory-service.service.ts
+++ src/app/shared/services/in-memory-service.service.ts
@@ -1,40 +1,59 @@
-import {Injectable} from '@angular/core';
-import {InMemoryDbService} from 'angular-in-memory-web-api';
-import {Observable} from 'rxjs';
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class InMemoryService implements InMemoryDbService{
-
-  constructor() { }
+export class InMemoryService implements InMemoryDbService {
+  constructor() {}
 
   createDb(): {} | Observable<{}> | Promise<{}> {
     const products = [
-      {id: 1, name: 'Product One', gender: 'Male', size: 'M'},
-      {id: 2, name: 'Product Two', gender: 'Female', size: 'S'},
-      {id: 3, name: 'Product Three', gender: 'Male', size: 'XXL'},
-      {id: 3, name: 'Product Four', gender: 'Female', size: 'XL'},
-      {id: 3, name: 'Product Five', gender: 'Male', size: 'XXL'},
-      {id: 3, name: 'Product Six', gender: 'Male', size: 'L'},
-      {id: 3, name: 'Product Seven', gender: 'Female', size: 'XXXL'},
-      {id: 3, name: 'Product Eight', gender: 'Male', size: 'M'},
-      {id: 3, name: 'Product Nine', gender: 'Female', size: 'S'},
-      {id: 3, name: 'Product Ten', gender: 'Female', size: 'L'}
+      { productId: 1, name: 'Product One', gender: 'Male', size: 'M' },
+      { productId: 2, name: 'Product Two', gender: 'Female', size: 'S' },
+      { productId: 3, name: 'Product Three', gender: 'Male', size: 'XXL' },
+      { productId: 4, name: 'Product Four', gender: 'Female', size: 'XL' },
+      { productId: 5, name: 'Product Five', gender: 'Male', size: 'XXL' },
+      { productId: 6, name: 'Product Six', gender: 'Male', size: 'L' },
+      { productId: 7, name: 'Product Seven', gender: 'Female', size: 'XXXL' },
+      { productId: 8, name: 'Product Eight', gender: 'Male', size: 'M' },
+      { productId: 9, name: 'Product Nine', gender: 'Female', size: 'S' },
+      { productId: 10, name: 'Product Ten', gender: 'Female', size: 'L' },
     ];
     const prices = [
       {
-        id: 1,
+        priceId: 1,
         vendor: 'Amazon',
-        price: 'Rs. 499'
+        price: 'Rs. 499',
+        productId: 1,
       },
       {
-        id: 2,
+        priceId: 2,
         vendor: 'Flipkart',
-        price: 'Rs. 599'
-      }
+        price: 'Rs. 599',
+        productId: 1,
+      },
+      {
+        priceId: 3,
+        vendor: 'Flipkart 3',
+        price: 'Rs. 599',
+        productId: 2,
+      },
+      {
+        priceId: 4,
+        vendor: 'Flipkart 4',
+        price: 'Rs. 599',
+        productId: 3,
+      },
+      {
+        priceId: 5,
+        vendor: 'Flipkart 5',
+        price: 'Rs. 599',
+        productId: 4,
+      },
     ];
 
-    return {products, prices};
+    return { products, prices };
   }
 }
